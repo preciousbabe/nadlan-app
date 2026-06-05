fd267a0bab928769af152ceab4cda4dc2122a994
preciousbabe <ezechukwuifeoma279@gmail.com>
2026-06-05 21:09:25 +0100

    redated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { useState } from 'react'
 import AppRoutes from './routes/AppRoutes'
-import InstallPrompt from './components/InstallPrompt'
 
 function App() {
-  const [currentSection, setCurrentSection] = useState('real-estate')
+
+  const [currentSection, setCurrentSection] =
+    useState('real-estate')
 
   return (
-    <>
-      <AppRoutes
-        currentSection={currentSection}
-        setCurrentSection={setCurrentSection}
-      />
-
-      {/* 🔥 GLOBAL PWA INSTALL UI */}
-      <InstallPrompt />
-    </>
+    <AppRoutes
+      currentSection={currentSection}
+      setCurrentSection={setCurrentSection}
+    />
   )
 }
 
